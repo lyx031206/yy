74856add49b2c2b4f203e081ed369100ba998daa
lyx031206 <122373135+lyx031206@users.noreply.github.com>
2024-03-19 22:50:41 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -11,7 +11,10 @@ let addresses = [
 
 // 设置优选地址api接口
 let addressesapi = [
-	'https://cf-workers-text2kv-3ur.pages.dev/ipyx.txt?token=99aa4b32-db8a-4b0f-bed5-38bf0ecf8723' //可参考内容格式 自行搭建。
+	'https://cf-workers-text2kv-3ur.pages.dev/ipyx.txt?token=99aa4b32-db8a-4b0f-bed5-38bf0ecf8723',
+	'https://addressesapi.090227.xyz/ct',
+        'https://addressesapi.090227.xyz/cmcc',
+        'https://addressesapi.090227.xyz/CloudFlareYes',
 ];
 
 let DLS = 4;//速度下限
