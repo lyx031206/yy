bb1eea72952285e78b8df6b912d822aa54c62efe
lyx031206 <122373135+lyx031206@users.noreply.github.com>
2024-03-19 21:51:56 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -11,7 +11,7 @@ let addresses = [
 
 // 设置优选地址api接口
 let addressesapi = [
-	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt' //可参考内容格式 自行搭建。
+	'https://cf-workers-text2kv-3ur.pages.dev/ipyx.txt?token=99aa4b32-db8a-4b0f-bed5-38bf0ecf8723' //可参考内容格式 自行搭建。
 ];
 
 let DLS = 4;//速度下限
